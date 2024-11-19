62ec63df87efbcb317a50ead78c949bad541cc46
Obaid <obaid3035@gmail.com>
2024-11-19 15:39:55 +0500

    add to new-app-2 Commit

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def farewell(name):
     return f"Goodbye, {name}!"
 
 if __name__ == "__main__":
-    name = input("Enter your name: ")
+    name = input("Enter your name buddy: ")
     print(greet(name))
     print(farewell(name))
